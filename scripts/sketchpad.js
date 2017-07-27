ea77f0b7a7bb397b10ddb0a0c979b2786e154387
bagool <ioan.adrian98@gmail.com>
2017-07-27 13:25:03 +0300

    added user controls

--- scripts/sketchpad.js
+++ scripts/sketchpad.js
@@ -1,43 +1,68 @@
 red = green = blue = 255;
 
+grid = 16;
+
+gradientToggled = true;
+
 $(document).ready(() => {
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+  buildSquares();
+
+  $('button').on('click', function() {
+    let text = $(this).text();
+
+    text = (text == "Toggle gradient") ? "Toggle random colors" : "Toggle gradient";
+
+    $(this).text(text);
+
+    gradientToggled = (text == "Toggle gradient") ? false : true;
+
+    $('.square').css({'background-color': 'white'});
+  });
+
+  $('#size').change(function() {
+    grid = $(this).val();
+
+    if (grid < 1 || grid > 100) {
+      $('.error').text("You shouldn't enter a value that's not 1 or 100")
+                 .fadeToggle(200);
+
+      $('.error').delay(1000)
+                 .fadeToggle(3000);
+    } else {
+      $('.square').remove();
+      buildSquares();
+    }
+  });
+});
+
+function buildSquares() {
+  for (let i = 0; i < grid; i++) {
+    for (let j = 0; j < grid; j++) {
       $('.container').append('<div class="square"></div>');
     }
-    $('.container').append('<br/>');
   }
-    resizeSquares();
 
-    $('.square').hover(function() {
-      /* uncomment this one for random colors
-        red = Math.floor(Math.random() * 255);
-        green = Math.floor(Math.random() * 255);
-        blue = Math.floor(Math.random() * 255);
-      */
+  $('.square').hover(function() {
+    if (gradientToggled === true) {
+      if (red != 0) {
+        red--; green--; blue--;
+      } else {
+        red = green = blue = 255;
+      }
+    } else {
+      red = Math.floor(Math.random() * 255);
+      green = Math.floor(Math.random() * 255);
+      blue = Math.floor(Math.random() * 255);
+    }
 
-      /* uncomment this one to get white to black gradient
-        if (red != 0) {
-          red--; green--; blue--;
-        } else {
-          red = green = blue = 255;
-        }
-      */
+    $(this).css({
+      'transition': 'background-color 1s ease-out',
+      'background-color': 'rgb(' + red + ',' + green + ',' + blue + ')'
+    }, 'slow');
+  });
 
-      $(this).css({
-        'background-color': 'rgb(' + red + ',' + green + ',' + blue + ')'
-      })
-    });
-});
-
-$(window).on('resize', () => {
-  resizeSquares();
-});
-
-
-function resizeSquares() {
-  let width = $(window).width() / 16 - 3;
-  let height = $(window).height() / 16 - 3;
+  let width = 500 / grid;
+  let height = 500 / grid;
 
   $('.square').width(width).height(height);
 }
